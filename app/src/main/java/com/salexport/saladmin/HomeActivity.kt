75fed1d5a3ed9cc5f5cc9a1fc48b3aade070439e
Art <suardet.chph@gmail.com>
2019-08-05 10:23:16 +0700

    Add textviwe and navigation

--- app/src/main/java/com/salexport/saladmin/HomeActivity.kt
+++ app/src/main/java/com/salexport/saladmin/HomeActivity.kt
@@ -2,11 +2,22 @@ package com.salexport.saladmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private Button button8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
+
+    private fun onclick(View view){
+        intent i = new intent (this,MainActivity.class)
+    }
 }
+
+
